9c9b07246a878737f65c7b37bdade70236fb728b
Oleg Bashylov <olegbash94@gmail.com>
2024-07-26 18:10:36 +0300

    hero_DONE

--- src/js/app.js
+++ src/js/app.js
@@ -34,7 +34,7 @@ flsFunctions.isWebp();
 /* Додавання класу touch для HTML якщо браузер мобільний */
 // flsFunctions.addTouchClass();
 /* Додавання loaded для HTML після повного завантаження сторінки */
-// flsFunctions.addLoadedClass();
+flsFunctions.addLoadedClass();
 /* Модуль для роботи з меню (Бургер) */
 flsFunctions.menuInit();
 /* Враховування плаваючої панелі на мобільних пристроях при 100vh */
@@ -185,7 +185,7 @@ import './libs/select.js'
 // Спостерігач за об'єктами з атрибутом data-watch
 // Документація: https://template.fls.guru/template-docs/modul-nabljudatel-za-poyavleniem-elementa-pri-skrolle.html
 // Сніппет(HTML):
-// import './libs/watcher.js'
+import './libs/watcher.js'
 
 // Модуль поекранної прокрутки
 // Документація: https://template.fls.guru/template-docs/modul-poekrannoj-prokrutki-stranicy-fullpage.html
@@ -195,7 +195,7 @@ import './libs/select.js'
 // Модуль паралаксу
 // Документація: https://template.fls.guru/template-docs/paralaks-pri-skroli.html
 // Сніппет(HTML):
-// import './libs/parallax.js'
+import './libs/parallax.js'
 
 // Функції роботи скролом
 import * as flsScroll from "./files/scroll/scroll.js";
