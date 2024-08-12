da5ab3a96d13b4470bb0f86f2f2c4fcdb1ba7ce5
Oleg Bashylov <olegbash94@gmail.com>
2024-08-12 20:55:30 +0300

    about-why_htmlDONE

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -268,6 +268,88 @@ function initSliders() {
 		});
 	}
 	// ============================================================= >>>SLIDER-CAREERS =====
+
+	// ===== SLIDER-WHY ====================================================================
+
+	if (document.querySelector('.slider-why__slider')) { // Вказуємо склас потрібного слайдера
+		// Створюємо слайдер
+		new Swiper('.slider-why__slider', { // Вказуємо склас потрібного слайдера
+			// Підключаємо модулі слайдера
+			// для конкретного випадку
+			modules: [Navigation, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			//direction: "vertical",
+			//autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Ефекти
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагінація
+
+			pagination: {
+				el: '.slider-why__pagination',
+				type: "progressbar",
+			},
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "вліво/вправо"
+			navigation: {
+				prevEl: '.slider-why__button-prev',
+				nextEl: '.slider-why__button-next',
+			},
+			/*
+			// Брейкпоінти
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			// Події
+			on: {
+
+			}
+		});
+	}
+
+	// ================================================================= >>>SLIDER-WHY =====
 }
 
 // Button's link changing after active slide changing
